a7406ccfec7800cf57b1bb40fbf1bfb903960a97
adipermadi271-creator <adipermadi271@gmail.com>
2026-03-05 05:34:14 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.aiimage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
